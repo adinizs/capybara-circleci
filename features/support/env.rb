55c5d0f7647b655d897a4833106f871c8b9aff8c
diniz <anderson.silva@somospi.com.br>
2020-06-02 09:02:40 -0300

    ajuste circleci

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,8 +1,6 @@
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 require 'site_prism'
-require 'faker'
-require 'cpf_faker'
 
 Capybara.configure do |config|
     config.default_driver = :selenium_chrome
